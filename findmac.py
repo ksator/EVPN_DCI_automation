89ead0c7e0f0cb748aaa6588ce9b92ddfc7a8ea2
Khelil Sator <khelil.sator.git@gmail.com>
2017-10-08 22:50:09 +0200

    Update findmac.py

--- findmac.py
+++ findmac.py
@@ -6,13 +6,10 @@ it uses the ansible inventory file, login in all the device, and print where the
 # python ./findmac.py 38:4f:49:f2:5f:fc
 Device: QFX5100-48S-6
 interfaces list: ['ae0.0']
-
 Device: QFX5100-48S3-11
 interfaces list: ['ae0.0']
-
 Device: QFX5100-48S3-21
 interfaces list: ['ae2.0']
-
 Device: QFX5100-48S3-23
 interfaces list: ['ae2.0']
 '''
@@ -20,16 +17,12 @@ interfaces list: ['ae2.0']
 python ./findmac.py 00:25:05:00:00:01
 Device: Superfast-QFX
 interfaces list: ['esi.2164']
-
 Device: Dori-QFX
 interfaces list: ['esi.2170']
-
 Device: Theia-QFX
 interfaces list: ['esi.2149']
-
 Device: Nori-QFX
 interfaces list: ['esi.2156']
-
 '''
 from jnpr.junos import Device
 from lxml import etree
@@ -65,7 +58,7 @@ for dev_item in devices_list:
     dev=Device(host=dev_item, user="jnpr", password="pass123")
     dev.open()
     interface_list=[]
-    result=dev.rpc.get_ethernet_switching_table_information(normalize=True)
+    result=dev.rpc.get_ethernet_switching_table_information(normalize=True, address=sys.argv[1])
     #   type(result)
     #   print (etree.tostring(result))
     #   etree.dump(result)
@@ -73,16 +66,19 @@ for dev_item in devices_list:
     #   type (mac_list)
     #   len (mac_list)
     #   mac_list
-    # etree.dump(mac_list[0])
-    for mac_item in mac_list:
-        if (mac_item.findtext('l2ng-l2-mac-address') == sys.argv[1]) and (mac_item.findtext('l2ng-l2-mac-logical-interface') not in interface_list):
-            interface_list.append(mac_item.findtext('l2ng-l2-mac-logical-interface'))
-    if  interface_list!=[]:
-        print("Device: %s" %dev.facts['hostname'])
-        print("interfaces list: %s" % interface_list)
+    if len (mac_list) == 0:
         print("")
-    dev.close()
+        print "%s is not known by %s" %(sys.argv[1], dev.facts['hostname'])
+        dev.close()
+    else:
+    #   etree.dump(mac_list[0])
+        for mac_item in mac_list:
+            if  mac_item.findtext('l2ng-l2-mac-logical-interface') not in interface_list:
+                interface_list.append(mac_item.findtext('l2ng-l2-mac-logical-interface'))
+        print("")
+        print "%s is known by %s via the list of interfaces %s" %(sys.argv[1], dev.facts['hostname'],  interface_list)
+        dev.close()
 
 devices_list_size=len(devices_list)
+print("")
 print "lookup done accross %s devices." %devices_list_size
-
